f6f0fbc290db366408db73a4e262c14d94d1938d
Julius <julius.simonelli@gmail.com>
2021-12-21 19:07:22 -0800

    init

---
+++ test_struct.py
@@ -0,0 +1,44 @@
+from pyxtend import struct
+import numpy as np
+
+small_set = {1, "this"}
+res = struct(small_set)
+assert str(res) == "{<class 'set'>: [<class 'int'>, <class 'str'>]}"
+
+large_set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+res = struct(large_set)
+assert str(res) == "{<class 'set'>: [<class 'int'>, <class 'int'>, <class 'int'>, '...10 total']}"
+
+res = struct(range(10))
+assert str(res) == "{<class 'range'>: [<class 'int'>, <class 'int'>, <class 'int'>, '...10 total']}"
+
+res = struct(range(0))
+assert str(res) == "{<class 'range'>: []}"
+
+long_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+res = struct(long_list)
+assert str(res) == "{<class 'list'>: [<class 'int'>, <class 'int'>, <class 'int'>, '...9 total']}"
+
+
+mixed_list = [1, "this", "sam", [4, 3, "here"]]
+res = struct(mixed_list)
+assert str(res) == "{<class 'list'>: [<class 'int'>, <class 'str'>, <class 'str'>, '...4 total']}"
+
+
+recursive_list = [1, [[1, 2], 3, "here"]]
+res = struct(recursive_list)
+assert str(res) == "{<class 'list'>: [<class 'int'>, <class 'list'>]}"
+
+recursive_list = np.array([1, 2, 3, 4, 5, 6, 7])
+res = struct(recursive_list)
+assert (
+    str(res)
+    == "{<class 'numpy.ndarray'>: [<class 'numpy.int32'>, <class 'numpy.int32'>, <class 'numpy.int32'>, '...7 total']}"
+)
+
+recursive_list = np.zeros((5, 5, 5))
+res = struct(recursive_list)
+assert (
+    str(res)
+    == "{<class 'numpy.ndarray'>: [<class 'numpy.ndarray'>, <class 'numpy.ndarray'>, <class 'numpy.ndarray'>, '...5 total']}"
+)
