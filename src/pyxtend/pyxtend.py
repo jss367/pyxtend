c2b13c48dbf7f6f335ba96e1201d7b5b5bc904fb
Julius <julius.simonelli@gmail.com>
2023-05-09 17:56:49 -0700

    Update code and tests

--- src/pyxtend/pyxtend.py
+++ src/pyxtend/pyxtend.py
@@ -1,25 +1,48 @@
 import contextlib
-from typing import Iterable
+from collections.abc import Iterable
+from typing import Any, Iterable, Union
+
+import numpy as np
+import tensorflow as tf
+import torch
 
 
-def struct(obj, limit=3):
-    "Return the general structure of any object"
-    if isinstance(obj, str) or not isinstance(obj, Iterable):
-        return type(obj)
-    if isinstance(obj, set):
-        c = iter(obj)
-        a = []
-        for _ in range(min(limit, len(obj))):
-            a.append(type(next(c, None)))
-        if len(obj) > limit:
-            a.append(f"...{len(obj)} total")
-        return {type(obj): a}
-    if isinstance(obj, tf.Tensor):
-        return obj.shape
-    a = [struct(obj_) for obj_ in obj[:limit]]
-    if len(obj) > limit:
-        a.append(f"...{len(obj)} total")
-    return {type(obj): a}
+def struct(obj: Any, level: int = 0, limit: int = 3) -> Union[str, dict]:
+    """
+    Returns the general structure of a given Python object.
+
+    Args:
+        obj: The Python object to analyze.
+        level: The current depth of recursion (default: 0).
+        limit: The maximum number of elements to display for each type (default: 3).
+
+    Returns:
+        The structure of the input object as a dictionary or string.
+    """
+    if isinstance(obj, (int, float, bool)):
+        return type(obj).__name__
+    elif isinstance(obj, str):
+        return "str"
+    elif isinstance(obj, torch.Tensor):
+        return {f"{type(obj).__name__}": [f"{obj.dtype}, shape={tuple(obj.shape)}"]}
+    elif isinstance(obj, tf.Tensor):
+        return {f"{type(obj).__name__}": [f"{obj.dtype.name}, shape={tuple(obj.shape)}"]}
+    elif isinstance(obj, np.ndarray):
+        if obj.size == 0:  # Check if the array is empty
+            inner_structure = "empty"
+        else:
+            inner_structure = struct(obj.item(0), level + 1)
+        return {f"{type(obj).__name__}": [f"{obj.dtype}, shape={obj.shape}"]}
+    elif isinstance(obj, Iterable) and not isinstance(obj, (str, bytes)):
+        if level < limit:
+            inner_structure = [struct(x, level + 1) for x in obj]
+            if len(obj) > 3:
+                inner_structure = inner_structure[:3] + [f"...{len(obj)} total"]
+            return {type(obj).__name__: inner_structure}
+        else:
+            return {type(obj).__name__: "..."}
+    else:
+        return "unsupported"  # type(obj).__name__ or {type(obj).__name__: 'unsupported'}
 
 
 def vprint(*text: str) -> None:
--- tests/unit/test_struct.py
+++ tests/unit/test_struct.py
@@ -1,70 +1,105 @@
 import numpy as np
+import pandas as pd
+import tensorflow as tf
+
 from src.pyxtend import struct
 
-small_set = {1, "this"}
-res = struct(small_set)
-# the order here is not determined, so it could go either way
-# assert str(res) == "{<class 'set'>: [<class 'int'>, <class 'str'>]}"
-# "{<class 'set'>: [<class 'str'>, <class 'int'>]}"
 
-large_set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-res = struct(large_set)
-assert str(res) == "{<class 'set'>: [<class 'int'>, <class 'int'>, <class 'int'>, '...10 total']}"
-"{<class 'set'>: [<class 'int'>, <class 'int'>, <class 'int'>]}"
-
-res = struct(range(10))
-assert str(res) == "{<class 'range'>: [<class 'int'>, <class 'int'>, <class 'int'>, '...10 total']}"
-
-res = struct(range(0))
-assert str(res) == "{<class 'range'>: []}"
-
-long_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-res = struct(long_list)
-assert str(res) == "{<class 'list'>: [<class 'int'>, <class 'int'>, <class 'int'>, '...9 total']}"
+def test_string():
+    res = struct("Hello, world!")
+    assert res == "str"
 
 
-mixed_list = [1, "this", "sam", [4, 3, "here"]]
-res = struct(mixed_list)
-assert str(res) == "{<class 'list'>: [<class 'int'>, <class 'str'>, <class 'str'>, '...4 total']}"
+def test_small_list():
+    res = struct([1, 2, 3])
+    assert res == {"list": ["int", "int", "int"]}
 
 
-recursive_list = [1, [[1, 2], 3, "here"]]
-res = struct(recursive_list)
-assert (
-    str(res)
-    == "{<class 'list'>: [<class 'int'>, {<class 'list'>: [{<class 'list'>: [<class 'int'>, <class 'int'>]}, <class 'int'>, <class 'str'>]}]}"
-)
+def test_empty_list():
+    res = struct([])
+    assert res == {"list": []}
 
 
-recursive_list = np.array([1, 2, 3, 4, 5, 6, 7])
-res = struct(recursive_list)
-assert (
-    str(res)
-    == "{<class 'numpy.ndarray'>: [<class 'numpy.int32'>, <class 'numpy.int32'>, <class 'numpy.int32'>, '...7 total']}"
-)
+def test_small_set():
+    small_set = {1, "this"}
+    res = struct(small_set)
+    assert res == {"set": ["int", "str"]} or res == {"set": ["str", "int"]}
 
-# np_array = np.zeros((5, 5, 5))
-# res = struct(np_array)
-# assert (
-#     str(res)
-#     == "{<class 'numpy.ndarray'>: [<class 'numpy.ndarray'>, <class 'numpy.ndarray'>, <class 'numpy.ndarray'>, '...5 total']}"
-# )
-# "{<class 'numpy.ndarray'>: [{<class 'numpy.ndarray'>: [{<class 'numpy.ndarray'>: [<class 'numpy.float64'>, <class 'numpy.float64'>, <class 'numpy.float64'>, '...5 total']}, {<class 'numpy.ndarray'>: [<class 'numpy.float64'>, <class 'numpy.float64'>, <class 'numpy.float64'>, '...5 total']}, {<class 'numpy.ndarray'>: [<class 'numpy.float64'>, <class 'numpy.float64'>, <class 'numpy.float64'>, '...5 total']}, '...5 total']}, {<class 'numpy.ndarray'>: [{<class 'numpy.ndarray'>: [<class 'numpy.float64'>, <class 'numpy.float64'>, <class 'numpy.float64'>, '...5 total']}, {<class 'numpy.ndarray'>: [<class 'numpy.float64'>, <class 'numpy.float64'>, <class 'numpy.float64'>, '...5 total']}, {<class 'numpy.ndarray'>: [<class 'numpy.float64'>, <class 'numpy.float64'>, <class 'numpy.float64'>, '...5 total']}, '...5 total']}, {<class 'numpy.ndarray'>: [{<class 'numpy.ndarray'>: [<class 'numpy.float64'>, <class 'numpy.float64'>, <class 'numpy.float64'>, '...5 total']}, {<class 'numpy.ndarray'>: [<class 'numpy.float64'>, <class 'numpy.float64'>, <class 'numpy.float64'>, '...5 total']}, {<class 'numpy.ndarray'>: [<class 'numpy.float64'>, <class 'numpy.float64'>, <class 'numpy.float64'>, '...5 total']}, '...5 total']}, '...5 total']}"
 
-# huge_array = np.zeros((10000, 5, 256, 256, 3))
-# res = struct(huge_array)
-# assert (
-#     str(res)
-#     == "{<class 'numpy.ndarray'>: [<class 'numpy.ndarray'>, <class 'numpy.ndarray'>, <class 'numpy.ndarray'>, '...5 total']}"
-# )
+def test_large_set():
+    large_set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    res = struct(large_set)
+    assert res == {"set": ["int", "int", "int", "...10 total"]}
+
+
+def test_empty_range():
+    res = struct(range(0))
+    assert res == {"range": []}
+
+
+def test_range():
+    res = struct(range(10))
+    assert res == {"range": ["int", "int", "int", "...10 total"]}
+
+
+def test_long_list():
+    long_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    res = struct(long_list)
+    assert res == {"list": ["int", "int", "int", "...9 total"]}
+
+
+def test_mixed_list():  # udpate this one
+    mixed_list = [1, "this", "sam", [4, 3, "here"]]
+    res = struct(mixed_list)
+    assert res == {"list": ["int", "str", "str", "...4 total"]}
+
+
+def test_list_in_list():  # update this one
+    list_in_list = [1, [[1, 2], 3, "here"]]
+    res = struct(list_in_list)
+    assert res == {"list": ["int", {"list": [{"list": ["int", "int"]}, "int", "str"]}]}
+
+
+def test_numpy_array():  # maybe include three?
+    np_arr = np.array([1, 2, 3, 4, 5, 6, 7])
+    res = struct(np_arr)
+    assert res == {"ndarray": ["int32, shape=(7,)"]}
+
+
+def test_tuple():
+    test_tuple = (1, 2, 3, 4, 5, 6)
+    res = struct(test_tuple)
+    assert res == {"tuple": ["int", "int", "int", "...6 total"]}
+
+
+def test_numpy_3d():
+    np_array = np.zeros((5, 5, 5))
+    res = struct(np_array)
+    assert res == {"ndarray": ["float64, shape=(5, 5, 5)"]}
+
+
+def test_empty_numpy():
+    res = struct(np.array([]))
+    assert res == {"ndarray": ["float64, shape=(0,)"]}
 
 
 ## TF Tensors
 
-tensor = pd.Series([[tf.constant(1, shape=(1))],[tf.constant(1, shape=(1))],[tf.constant(1, shape=(1))], [tf.constant(1, shape=(1))]])
 
-res = struct(tensor)
+def test_tf_tensor():
+    tensor = pd.Series(
+        [
+            [tf.constant(1, shape=(1))],
+            [tf.constant(1, shape=(1))],
+            [tf.constant(1, shape=(1))],
+            [tf.constant(1, shape=(1))],
+        ]
+    )
 
-assert res == {pd.Series: [{list: [tf.TensorShape([1])]}, {list: [tf.TensorShape([1])]}, {list: [tf.TensorShape([1])]}, '...4 total']}
+    res = struct(tensor)
 
-
+    assert (
+        str(res)
+        == "{'Series': [{'list': [{'EagerTensor': ['int32, shape=(1,)']}]}, {'list': [{'EagerTensor': ['int32,"
+        " shape=(1,)']}]}, {'list': [{'EagerTensor': ['int32, shape=(1,)']}]}, '...4 total']}"
+    )
