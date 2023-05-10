f16bc3621e214ad921c961b249ef7e269f4975ed
Julius <julius.simonelli@gmail.com>
2023-05-09 18:07:52 -0700

    Update test_struct.py

--- tests/unit/test_struct.py
+++ tests/unit/test_struct.py
@@ -1,92 +1,123 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import torch
 
 from src.pyxtend import struct
 
 
 def test_string():
-    res = struct("Hello, world!")
-    assert res == "str"
+    result = struct("Hello, world!")
+    assert result == "str"
 
 
 def test_small_list():
-    res = struct([1, 2, 3])
-    assert res == {"list": ["int", "int", "int"]}
+    result = struct([1, 2, 3])
+    assert result == {"list": ["int", "int", "int"]}
 
 
 def test_empty_list():
-    res = struct([])
-    assert res == {"list": []}
+    result = struct([])
+    assert result == {"list": []}
 
 
 def test_small_set():
     small_set = {1, "this"}
-    res = struct(small_set)
-    assert res == {"set": ["int", "str"]} or res == {"set": ["str", "int"]}
+    result = struct(small_set)
+    assert result == {"set": ["int", "str"]} or result == {"set": ["str", "int"]}
 
 
 def test_large_set():
     large_set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-    res = struct(large_set)
-    assert res == {"set": ["int", "int", "int", "...10 total"]}
+    result = struct(large_set)
+    assert result == {"set": ["int", "int", "int", "...10 total"]}
+
+
+def test_empty_set():
+    result = struct(set())
+    assert result == {"set": []}
 
 
 def test_empty_range():
-    res = struct(range(0))
-    assert res == {"range": []}
+    result = struct(range(0))
+    assert result == {"range": []}
 
 
 def test_range():
-    res = struct(range(10))
-    assert res == {"range": ["int", "int", "int", "...10 total"]}
+    result = struct(range(10))
+    assert result == {"range": ["int", "int", "int", "...10 total"]}
 
 
 def test_long_list():
     long_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    res = struct(long_list)
-    assert res == {"list": ["int", "int", "int", "...9 total"]}
+    result = struct(long_list)
+    assert result == {"list": ["int", "int", "int", "...9 total"]}
 
 
 def test_mixed_list():  # udpate this one
     mixed_list = [1, "this", "sam", [4, 3, "here"]]
-    res = struct(mixed_list)
-    assert res == {"list": ["int", "str", "str", "...4 total"]}
+    result = struct(mixed_list)
+    assert result == {"list": ["int", "str", "str", "...4 total"]}
 
 
 def test_list_in_list():  # update this one
     list_in_list = [1, [[1, 2], 3, "here"]]
-    res = struct(list_in_list)
-    assert res == {"list": ["int", {"list": [{"list": ["int", "int"]}, "int", "str"]}]}
+    result = struct(list_in_list)
+    assert result == {"list": ["int", {"list": [{"list": ["int", "int"]}, "int", "str"]}]}
 
 
 def test_numpy_array():  # maybe include three?
     np_arr = np.array([1, 2, 3, 4, 5, 6, 7])
-    res = struct(np_arr)
-    assert res == {"ndarray": ["int32, shape=(7,)"]}
+    result = struct(np_arr)
+    assert result == {"ndarray": ["int32, shape=(7,)"]}
+
+
+def test_dict():
+    result = struct(({"a": 1, "b": 2}))
+    assert result == {"dict": ["str", "str"]}
+
+
+def test_large_dict():
+    result = struct(({"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}))
+    assert result == {"dict": ["str", "str", "str", "...5 total"]}
 
 
 def test_tuple():
     test_tuple = (1, 2, 3, 4, 5, 6)
-    res = struct(test_tuple)
-    assert res == {"tuple": ["int", "int", "int", "...6 total"]}
+    result = struct(test_tuple)
+    assert result == {"tuple": ["int", "int", "int", "...6 total"]}
 
 
 def test_numpy_3d():
     np_array = np.zeros((5, 5, 5))
-    res = struct(np_array)
-    assert res == {"ndarray": ["float64, shape=(5, 5, 5)"]}
+    result = struct(np_array)
+    assert result == {"ndarray": ["float64, shape=(5, 5, 5)"]}
 
 
 def test_empty_numpy():
-    res = struct(np.array([]))
-    assert res == {"ndarray": ["float64, shape=(0,)"]}
+    result = struct(np.array([]))
+    assert result == {"ndarray": ["float64, shape=(0,)"]}
+
+
+## PyTorch Tensors
+
+
+def test_torch_tensor():
+    torch_tensor = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], dtype=torch.float32)
+    result = struct(torch_tensor)
+    assert result == {"Tensor": ["torch.float32, shape=(2, 3)"]}
 
 
 ## TF Tensors
 
 
 def test_tf_tensor():
+    tf_tensor = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], dtype=tf.float32)
+    result = struct(tf_tensor)
+    assert result == {"EagerTensor": ["float32, shape=(2, 3)"]}
+
+
+def test_tf_tensors():
     tensor = pd.Series(
         [
             [tf.constant(1, shape=(1))],
@@ -96,10 +127,10 @@ def test_tf_tensor():
         ]
     )
 
-    res = struct(tensor)
+    result = struct(tensor)
 
     assert (
-        str(res)
+        str(result)
         == "{'Series': [{'list': [{'EagerTensor': ['int32, shape=(1,)']}]}, {'list': [{'EagerTensor': ['int32,"
         " shape=(1,)']}]}, {'list': [{'EagerTensor': ['int32, shape=(1,)']}]}, '...4 total']}"
     )
